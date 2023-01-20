27fe646be133a4a34e247949a49f2f30d88fb0a6
vikas <banagevikas@gmail.com>
2023-01-20 18:14:31 +0100

    added version

--- version.tf
+++ version.tf
@@ -7,6 +7,12 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket = "cloudmagic-terraform-state-bucket"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
